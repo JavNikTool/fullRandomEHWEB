f803490d8ca808800ef2753486a515672675c43b
JavNikTool <Nikitakremnev0@gmail.com>
2023-06-06 15:28:22 +0300

    v0.1

--- src/js/script.js
+++ src/js/script.js
@@ -7,32 +7,65 @@ document.addEventListener('DOMContentLoaded', () => {
         questions_circle = document.querySelectorAll('.select'),
         first_checkbox = document.getElementById('checkbox1');
 
-    questions.forEach((elem,i) => {
 
-        elem.addEventListener('mouseover', () => {
+
+
+
+    questions.forEach((e,i) => {
+        let addHover = function () {
             questions_circle.forEach((elem, i2) =>{
                 if(i == i2){
                     elem.style.cssText = 'transform: scale(0.9); background-color:white;';
                 }
             })
-        })
-
-        elem.addEventListener('mouseout', () => {
+        }
+        let remHover = function (){
             questions_circle.forEach((elem, i2) =>{
                 if(i == i2){
                     elem.style.cssText = 'transform: scale(1); background-color:none;';
                 }
             })
+        }
+
+        e.addEventListener('mouseover', addHover)
+        e.addEventListener('mouseout', remHover)
+
+        questions_display.forEach((item, key) => {
+            if(item.classList.contains('active'))
+            {
+                questions_circle.forEach((elem, i2) =>{
+                    if(key == i2){
+                        e.removeEventListener('mouseout', remHover)
+                        elem.style.cssText = 'transform: scale(0.9); background-color:white;';
+                    }
+                })
+            }
         })
 
-        elem.addEventListener('click',() => {
+
+
+        e.addEventListener('click',() => {
             questions_display.forEach((elem,i2) => {
                 if(i == i2){
                     elem.classList.toggle('active');
+                    if(elem.classList.contains('active'))
+                    {
+                        questions_circle.forEach((elem, i2) =>{
+                            if(i == i2){
+                                e.removeEventListener('mouseout', remHover)
+                                elem.style.cssText = 'transform: scale(0.9); background-color:white;';
+                            }
+                        })
+                    }
+                    else {
+                        e.addEventListener('mouseout', remHover)
+                    }
                 }
             })
         })
     })
+
+
     if(first_checkbox.checked === false)
         first_checkbox.checked = true;
 
